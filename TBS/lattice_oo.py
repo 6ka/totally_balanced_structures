0ea61eccf69de08c72a32883ef7c131c5acbc33d
Célia Châtel <chatelcelia@gmail.com>
2017-08-23 12:54:15 +0200

    lattice_oo : basis + inf/sup irreducibles

--- TBS/lattice_oo.py
+++ TBS/lattice_oo.py
@@ -1,16 +1,20 @@
 from TBS.observer import Observable
 from TBS.graph import Graph
 
+__author__ = "cchatel"
+
 
 class Lattice(Graph, Observable):
+    """Lattice class seen as the oriented cover graph of the actual lattice"""
+
     def __init__(self, vertices=tuple(), edges=tuple(), dual=None):
         Observable.__init__(self)
         Graph.__init__(self, directed=True)
         if dual is None:
-            self.dual = Lattice(dual=self)
+            self.dual_lattice = Lattice(dual=self)
         else:
-            self.dual = dual
-        self.attach(self.dual)
+            self.dual_lattice = dual
+        self.attach(self.dual_lattice)
         self._neighborhood = {}
 
         for x in vertices:
@@ -20,9 +24,168 @@ class Lattice(Graph, Observable):
             self.update(edges)
 
     def update(self, edges=tuple(), node_creation=True, delete=True):
+        """Add/remove edges and keep dual object up to date.
+
+        Each edge in *edges* is either added or removed depending if it
+        already present or not.
+
+        :param edges: Each edge is a pair `(x, y)`
+        :type edges: iterable
+
+        :param node_creation: If :const:`False`, edges connecting vertices
+                             not in the graph are discarded.
+                             If :const:`True`, missing vertices are added
+                             before adding the edge.
+        :type node_creation:  :class:`bool`
+
+        :param delete: If :const:`False` edges already present are not
+                       deleted from the graph.
+        :type delete: :class:`bool`
+
+        :raises: :exc:`ValueError` if the two vertices of an edge are equal.
+        """
         Graph.update(self, edges, node_creation=True, delete=True)
-        for obs in self.observers:
-            obs.dual_update(edges)
+        self.notify(edges)
 
     def dual_update(self, edges):
+        """Add/remove edges in dual lattice.
+
+        :param edges: Each edge is a pair `(x, y)`
+        """
         Graph.update(self, ((y, x) for (x, y) in edges))
+
+    def get_top(self):
+        """Return the largest element.
+
+        :param lattice: a lattice
+        :type lattice: :class:`TBS.graph.Graph`
+
+        :rtype: a element of the lattice.
+        """
+        for x in self:
+            if not self[x]:
+                return x
+
+        return None
+
+    def get_bottom(self):
+        """Return the smallest element.
+
+        :param lattice: a lattice
+        :type lattice: TBS.graph.Graph :class:`TBS.graph.Graph`
+
+        :rtype: a element of the lattice.
+        """
+
+        return self.dual_lattice.get_top()
+
+    def get_order(self):
+        """Return the order associated with the lattice.
+
+        :param lattice: a lattice
+        :type lattice: TBS.graph.Graph :class:`TBS.graph.Graph`
+
+        :rtype: TBS.graph.Graph :class:`TBS.graph.Graph`
+        """
+
+        bottom = self.get_bottom()
+
+        dual_order = Graph([bottom], directed=True)
+        for vertex in self.topological_sort(bottom):
+            for cover in self.dual_lattice[vertex]:
+                dual_order.update([(vertex, cover)])
+                dual_order.update([(vertex, y) for y in dual_order[cover]], delete=False)
+        return dual_order.dual()
+
+    def comparability_function(self):
+        """Return a comparability function associated with the lattice.
+
+        The return function takes two parameters and returns True if the first parameter is smaller than the second one
+        for the given lattice.
+        This function computes the lattice order (long). Should only be used for generic lattices where no other solution
+        is avaliable.
+
+        :param lattice: a lattice
+        :type lattice: TBS.graph.Graph :class:`TBS.graph.Graph`
+
+        :rtype: function
+        """
+        lattice_order = self.get_order()
+
+        def smaller_than(smaller, larger):
+            """Comparability function.
+
+            :param smaller: lattice element.
+            :param larger: lattice element.
+
+            :rtype: bool :class:`bool`
+            """
+
+            return larger in lattice_order[smaller]
+
+        return smaller_than
+
+    def inf_irreducible(self):
+        """ Inf-irreductibles elements of the cover graph.
+
+        :param lattice: a cover graph (may or may not have a bottom).
+        :type lattice: :class:`TBS.graph.Graph`
+
+        :return: the inf-irreducibles elements of *cover_graph*
+        :rtype: :class:`frozenset`.
+        """
+        irreducible = set()
+        for vertex in self:
+            if len(self[vertex]) == 1:
+                irreducible.add(vertex)
+
+        return frozenset(irreducible)
+
+    def sup_irreducible(self):
+        """ Sup-irreductibles elements of the cover graph.
+
+        :param cover_graph: a cover graph (may or may not have a top).
+        :type cover_graph: :class:`TBS.graph.Graph`
+
+        :return: the sup-irreducibles elements of *cover_graph*
+        :rtype: :class:`frozenset`.
+        """
+
+        return self.dual_lattice.inf_irreducible()
+
+    def sup_irreducible_clusters(self):
+        """ Sup-irreducibles correspondance.
+
+        :param lattice: a cover graph.
+        :type lattice: TBS.graph.Graph :class:`TBS.graph.Graph`
+
+        :return: a dict associating each element to the sup-irreducible elements smaller than him.
+        :rtype: :class:`dict`.
+        """
+
+        bottom = self.get_bottom()
+
+        correspondance = {bottom: set()}
+
+        for vertex in self.topological_sort(bottom):
+            if vertex not in correspondance:
+                correspondance[vertex] = set()
+            if self.dual_lattice.isa_leaf(vertex):
+                # sup_irreducible
+                correspondance[vertex].add(vertex)
+            for cover in self.dual_lattice[vertex]:
+                correspondance[vertex].update(correspondance[cover])
+
+        return {element: frozenset(sups) for element, sups in correspondance.items()}
+
+    def inf_irreducible_clusters(self):
+        """ Inf-irrerducibles correspondance.
+
+        :param lattice: a cover graph.
+        :type lattice: TBS.graph.Graph :class:`TBS.graph.Graph`
+
+        :return: a dict associating each element to the inf-irreducible elements smaller than him.
+        :rtype: :class:`dict`.
+        """
+
+        return self.dual_lattice.sup_irreducible_clusters()
--- tests/src/lattice/test_lattice_oo.py
+++ tests/src/lattice/test_lattice_oo.py
@@ -1,5 +1,90 @@
 from TBS.lattice_oo import Lattice
+import unittest
 
 
-lattice = Lattice()
+class TestCoverGraph(unittest.TestCase):
+    @staticmethod
+    def new_lattice():
+        lattice = Lattice()
+        lattice.update([("bottom", 1),
+                        ("bottom", 2),
+                        ("bottom", 3),
+                        ("bottom", 4),
+                        (1, 5),
+                        (2, 5),
+                        (2, 6),
+                        (2, 7),
+                        (3, 6),
+                        (4, 7),
+                        (5, 8),
+                        (6, 8),
+                        (7, 9),
+                        (8, "top"),
+                        (9, "top")])
+        return lattice
 
+    def setUp(self):
+        self.lattice = self.new_lattice()
+
+    def test_init(self):
+        self.assertSetEqual(frozenset(self.lattice.edges()), {("bottom", 1),
+                                                    ("bottom", 2), ("bottom", 3), ("bottom", 4), (1, 5), (2, 5), (2, 6),
+                                                    (2, 7), (3, 6), (4, 7), (5, 8), (6, 8), (7, 9), (8, "top"),
+                                                    (9, "top")})
+        self.assertSetEqual(frozenset(self.lattice.dual_lattice.edges()), {(1, "bottom"),
+                                                                           (2, "bottom"), (3, "bottom"), (4, "bottom"), (5, 1), (5, 2), (6, 2),
+                                                                           (7, 2), (6, 3), (7, 4), (8, 5), (8, 6), (9, 7), ("top", 8),
+                                                                           ("top", 9)})
+
+    def test_update(self):
+        self.lattice.update((("bottom", 10), (10, 9)))
+        self.assertIn((10, 9), self.lattice.edges())
+        self.assertIn((9, 10), self.lattice.dual_lattice.edges())
+        self.assertIn(("bottom", 10), self.lattice.edges())
+        self.assertIn((10, "bottom"), self.lattice.dual_lattice.edges())
+
+    def test_get_top(self):
+        self.assertEqual(self.lattice.get_top(), "top")
+
+    def test_get_bottom(self):
+        self.assertEqual(self.lattice.get_bottom(), "bottom")
+
+    def test_get_order(self):
+        order = self.lattice.get_order()
+        self.assertEqual({"top", 1, 2, 3, 4, 5, 6, 7, 8, 9}, frozenset(order["bottom"]))
+        self.assertEqual({9, "top"}, set(order[7]))
+
+    def test_comparability_function(self):
+        smaller_than = self.lattice.comparability_function()
+        self.assertTrue(smaller_than("bottom", "top"))
+        self.assertTrue(smaller_than(1, 5))
+        self.assertFalse(smaller_than(5, 1))
+        self.assertFalse(smaller_than(7, 8))
+
+    def test_inf_irreducible(self):
+        self.assertSetEqual(self.lattice.inf_irreducible(), frozenset({1, 3, 4, 5, 6, 7, 8, 9}))
+
+    def test_sup_irreducible(self):
+        self.assertSetEqual(self.lattice.sup_irreducible(), frozenset({1, 2, 3, 4, 9}))
+
+    def test_sup_irreducible_clusters(self):
+        clusters = self.lattice.sup_irreducible_clusters()
+        self.assertSetEqual(clusters[5], frozenset({1, 2}))
+        self.assertSetEqual(clusters[8], frozenset({1, 2, 3}))
+        self.assertSetEqual(clusters["bottom"], frozenset())
+        self.assertSetEqual(clusters["top"], frozenset({1, 2, 3, 4, 9}))
+
+    def test_inf_irreducible_clusters(self):
+        clusters = self.lattice.inf_irreducible_clusters()
+        self.assertSetEqual(clusters["bottom"], frozenset({1, 3, 4, 5, 6, 7, 8, 9}))
+        self.assertSetEqual(clusters[5], frozenset({5, 8}))
+        self.assertSetEqual(clusters[8], frozenset({8}))
+        self.assertSetEqual(clusters["top"], frozenset())
+
+    def test_inf_irreducible_without_bottom(self):
+        self.lattice.remove("bottom")
+        self.assertEqual(frozenset([1, 3, 4, 5, 6, 7, 8, 9]), self.lattice.inf_irreducible())
+
+    def test_sup_irreducible_without_top(self):
+        self.lattice.remove("top")
+        self.assertEqual(frozenset([1, 2, 3, 4, 9]), self.lattice.sup_irreducible())
