d5fdf6a6cec5dd25bfbfc01bc77b7ed2951a09fe
Célia Châtel <celia.chatel@lif.univ-mrs.fr>
2017-08-24 16:14:13 +0200

    lattice_oo : draw 2d lattice and terminal print

--- TBS/lattice_oo.py
+++ TBS/lattice_oo.py
@@ -3,6 +3,11 @@ from TBS.graph import Graph
 from TBS.tree import radial_draw_tree
 import collections
 import random
+from TBS.clusters import ClusterLineFromMatrix
+from TBS.contextmatrix import ContextMatrix
+from matplotlib import pyplot
+import matplotlib
+from TBS.clusters.to_string import BoxesToString
 
 __author__ = "cchatel", "fbrucker"
 
@@ -540,8 +545,7 @@ class Lattice(Graph, Observable):
                     edges_to_update += ((predecessor, class_to_create),)
                     for neighbor in tree[predecessor]:
                         if self.sup_filter(neighbor).intersection(
-                                self.sup_filter(predecessor)) <= self.sup_filter(
-                                class_to_create):
+                                self.sup_filter(predecessor)) <= self.sup_filter(class_to_create):
                             edges_to_update += ((neighbor, predecessor), (neighbor, class_to_create))
                 else:
                     for neighbor in tree[predecessor]:
@@ -588,6 +592,199 @@ class Lattice(Graph, Observable):
             save = directory + str(len(trees) - 1)
         radial_draw_tree(trees[-1], self, highlighted_node={order[-1]}, show=show, save=save)
 
+    def hierarchical_height_from_lattice(self):
+        """ Decompose the lattice into hierarchy.
+
+        Every dismantable lattice can be decomposed into hierarchy.
+
+        :return: A :class:`dict` whose key are the hierarchical level (0 is the first hierarchy) and value the
+                 associated element of the lattice.
+
+
+        """
+        vertex_height = dict()
+        current_height = 0
+        next_neighbor = dict()
+        current_degree = {x: self.degree(x) for x in self}
+        sup_irreducibles = set(self.sup_irreducible())
+        dual_lattice = self.dual()
+        while sup_irreducibles:
+            current_chains = set()
+            deleted_sup_irreducibles = set()
+            for current_sup_irreducible in sup_irreducibles:
+                possible_chain = set()
+                vertex = current_sup_irreducible
+                is_hierarchical = False
+                while current_degree[vertex] <= 1:
+                    possible_chain.add(vertex)
+                    if current_degree[vertex] == 0 or vertex in current_chains:
+                        is_hierarchical = True
+                        break
+                    vertex = next_neighbor.get(vertex, self[vertex][0])
+                if is_hierarchical:
+                    deleted_sup_irreducibles.add(current_sup_irreducible)
+                    current_chains.update(possible_chain)
+
+            for x in current_chains:
+                vertex_height[x] = current_height
+                for y in dual_lattice[x]:
+                    current_degree[y] -= 1
+                    if current_degree[y] == 1:
+                        for z in self[y]:
+                            if z not in vertex_height:
+                                next_neighbor[y] = z
+                                break
+            current_height += 1
+
+            sup_irreducibles.difference_update(deleted_sup_irreducibles)
+            vertex_height["BOTTOM"] = max(vertex_height.values()) + 1
+        return vertex_height
+
+    def draw(self):
+        formal_context_lattice = self.to_box_lattice()
+        matrix = ContextMatrix.from_lattice(formal_context_lattice).matrix
+        point_transformation = point_transformation_square(len(matrix))
+        representant = {box: box[0] for box in formal_context_lattice if box not in ("BOTTOM", "TOP")}
+        representant["BOTTOM"] = (len(matrix), 0)
+        representant["TOP"] = (0, len(matrix))
+        objects = formal_context_lattice.sup_irreducible()
+        attributes = formal_context_lattice.inf_irreducible()
+        hierarchy_association = formal_context_lattice.hierarchical_height_from_lattice()
+
+        for elem in formal_context_lattice:
+            x, y = point_transformation(*representant[elem])
+            if elem in objects:
+                type = "^"
+            elif elem in attributes:
+                type = "v"
+            else:
+                type = "o"
+            if elem in objects and elem in attributes:
+                type = "d"
+            pyplot.scatter(x, y, marker=type, zorder=1, color=formal_context_lattice.edge_color(elem, elem),
+                           edgecolors='black')
+
+            for neighbor in formal_context_lattice[elem]:
+                x2, y2 = point_transformation(*representant[neighbor])
+                color = formal_context_lattice.edge_color(elem, neighbor)
+                if hierarchy_association[elem] != hierarchy_association[neighbor]:
+                    type = ":"
+                else:
+                    type = "-"
+                pyplot.plot([x, x2], [y, y2], color=color, zorder=0, linestyle=type)
+        pyplot.show()
+
+    def edge_color(self, vertex1, vertex2=None):
+        hierarchy_association = self.hierarchical_height_from_lattice()
+        number_hierarchies = max(hierarchy_association.values()) + 1
+        colors = matplotlib.cm.rainbow([0. + 1.0 * x / (number_hierarchies - 1) for x in range(number_hierarchies)])
+        if vertex2 is None:
+            vertex2 = vertex1
+
+        return colors[max(hierarchy_association[vertex1], hierarchy_association[vertex2])]
+
+    def print_boxes(self):
+        context_matrix = ContextMatrix.from_lattice(self)
+        lattice = self.to_box_lattice()
+        boxes = self.boxes()
+
+        return BoxesToString(boxes.values(),
+                             context_matrix.elements, context_matrix.attributes,
+                             {value: context_matrix.attributes[value[0][1]] for value in
+                              boxes.values()},
+                             lattice).run()
+
+    def boxes(self):
+        """ Boxes and cluster number correspondence.
+
+        A box is a couple ((l1, c1), (l2, c2)) where (l1, c1) is the top left corner (line, column) of the box and
+        (l2, c2) the bottom right corner.
+
+        :param matrix: doubly lexically ordered and Gamma free 0/1 matrix
+        :return: :class:`dict` with key= class number and value= the associated box
+        """
+        context_matrix = ContextMatrix.from_lattice(self).reorder_doubly_lexical_order()
+        matrix = context_matrix.matrix
+        cluster_correspondence = dict()
+
+        for i, line in enumerate(ClusterLineFromMatrix(matrix)):
+            for j, elem in enumerate(line):
+                if elem is None:
+                    continue
+                if elem not in cluster_correspondence:
+                    cluster_correspondence[elem] = ((i, j), (i, j))
+                else:
+                    begin, end = cluster_correspondence[elem]
+                    cluster_correspondence[elem] = (begin, (i, j))
+
+        return cluster_correspondence
+
+    def to_box_lattice(self):
+        context_matrix = ContextMatrix.from_lattice(self)
+        context_matrix.reorder_doubly_lexical_order()
+        box_lattice = Lattice()
+        bottom = self.get_bottom()
+        top = self.get_top()
+        context_matrix = context_matrix.matrix
+        cluster_correspondence = ClusterLineFromMatrix.boxes(context_matrix)
+
+        last_line = last_line_not_0_for_matrix(context_matrix)
+        last_clusters = [None] * len(context_matrix[0])
+        line_iterator = ClusterLineFromMatrix(context_matrix)
+
+        for i, current_line in enumerate(line_iterator):
+            for j, elem in enumerate(current_line):
+                if elem is None:
+                    continue
+
+            j = len(current_line) - 1
+            while j >= 0:
+                if current_line[j] is None or current_line[j] in box_lattice:
+                    j -= 1
+                    continue
+
+                current_cluster = current_line[j]
+                # connect to bottom
+                if i == last_line[j]:
+                    box_lattice.update([(bottom, cluster_correspondence[current_cluster])])
+
+                # successor in line
+                right_successor = True
+                j_next = j + 1
+                while j_next < len(current_line) and current_line[j_next] is None:
+                    j_next += 1
+
+                if j_next == len(current_line):
+                    right_successor = False
+                if i > 0 and line_iterator.previous_line[j] is not None:
+                    if j_next == len(current_line) or current_line[j_next] == line_iterator.previous_line[j_next]:
+                        right_successor = False
+                if right_successor:
+                    box_lattice.update([(cluster_correspondence[current_cluster],
+                                         cluster_correspondence[current_line[j_next]])])
+
+                # successor before line
+                while j >= 0 and current_line[j] == current_cluster:
+                    if last_clusters[j] is not None and last_clusters[j] != current_cluster:
+                        box_lattice.update([(cluster_correspondence[current_cluster],
+                                             cluster_correspondence[last_clusters[j]])])
+                        successor = last_clusters[j]
+                        while j >= 0 and last_clusters[j] == successor:
+                            j -= 1
+                    else:
+                        break
+
+                while j >= 0 and current_line[j] == current_cluster:
+                    j -= 1
+
+            last_clusters = [new_cluster or old_cluster for new_cluster, old_cluster in
+                             zip(current_line, last_clusters)]
+
+        for vertex in set(x for x in box_lattice if box_lattice.degree(x) == 0):
+            box_lattice.update([(vertex, top)])
+
+        return box_lattice
+
 
 def max_intersection(antichain):
     n_elements = len(antichain)
@@ -609,3 +806,16 @@ def max_intersection(antichain):
         k += 1
     return indices_map[i], indices_map[j]
 
+
+def point_transformation_square(max_y):
+    return lambda line, column: (column, max_y - line)
+
+
+def last_line_not_0_for_matrix(matrix):
+    last_line = [-1] * len(matrix[0])
+    for j in range(len(matrix[0])):
+        for i in range(len(matrix) - 1, -1, -1):
+            if matrix[i][j] == 1:
+                last_line[j] = i
+                break
+    return last_line
