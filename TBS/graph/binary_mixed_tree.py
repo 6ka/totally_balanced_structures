e95cae5799c0b0da7ac88c16286cc17c5af55031
Célia Châtel <celia.chatel@lif.univ-mrs.fr>
2017-09-08 11:04:22 +0200

    binary_mixed_tree : drawing utilities

--- TBS/graph/binary_mixed_tree.py
+++ TBS/graph/binary_mixed_tree.py
@@ -1,4 +1,9 @@
 from TBS.graph.mixed_graph import MixedGraph
+from matplotlib import pyplot
+import matplotlib
+from TBS.graph import Graph
+import random
+import math
 
 
 class BinaryMixedTree(MixedGraph):
@@ -55,3 +60,83 @@ class BinaryMixedTree(MixedGraph):
         for z in edges:
             self.remove_undirected(x, z)
             self.add_undirected(y, z)
+
+    def to_graph(self):
+        tree = Graph(directed=False)
+        for vertex in self.vertices:
+            for neighbour in self.directed[vertex]:
+                if not tree.isa_edge(neighbour, vertex):
+                    tree.update(((vertex, neighbour),))
+            for neighbour in self.undirected[vertex]:
+                if not tree.isa_edge(neighbour, vertex):
+                    tree.update(((vertex, neighbour),))
+        return tree
+
+    def find_root_as_undirected(self):
+        pruned_tree = self.to_graph()
+        while len(pruned_tree) > 2:
+            leaves = [vertex for vertex in pruned_tree if pruned_tree.isa_leaf(vertex)]
+            for leaf in leaves:
+                pruned_tree.remove(leaf)
+        possible_roots = [root for root in pruned_tree]  # 1 or 2 possibilities
+        return random.choice(possible_roots)
+
+    def get_radial_tree_coordinates(self, root=None, order=None):
+        tree = self.to_graph()
+        if len(tree) == 1:
+            return {list(tree)[0]: [0, 0]}
+        if not root:
+            root = self.find_root_as_undirected()
+        if not order:
+            order = list(tree.topological_sort(root))
+        angles = {}
+        leaves = [vertex for vertex in order if tree.isa_leaf(vertex) and vertex != root]
+        for index, leaf in enumerate(leaves):
+            angles[leaf] = 2 * math.pi * index / len(leaves)
+        for vertex in reversed(order):
+            if vertex not in angles:
+                neighbors_angles = [angles[neighbor] for neighbor in tree[vertex] if neighbor in angles]
+                angles[vertex] = sum(neighbors_angles) / len(neighbors_angles)
+        coordinates = {order[0]: [0, 0]}
+        for vertex in order[1:]:
+            i = 0
+            predecessor = tree[vertex][i]
+            while predecessor not in coordinates:
+                predecessor = tree[vertex][i + 1]
+                i += 1
+            coordinates[vertex] = [coordinates[predecessor][0] + math.cos(angles[vertex]),
+                                   coordinates[predecessor][1] + math.sin(angles[vertex])]
+        return coordinates
+
+    def draw(self, highlighted_edge=set(), highlighted_node=set(), save=None, show=True):
+        fig, ax = pyplot.subplots()
+        root = self.find_root_as_undirected()
+        coordinates = self.get_radial_tree_coordinates(root)
+        lines = []
+        red_lines = []
+        green_lines = []
+        for vertex in self.vertices:
+            for neighbour in self.undirected[vertex]:
+                if (vertex, neighbour) not in highlighted_edge and (neighbour, vertex) not in highlighted_edge:
+                    lines.append([tuple(coordinates[vertex]), tuple(coordinates[neighbour])])
+                else:
+                    green_lines.append([tuple(coordinates[vertex]), tuple(coordinates[neighbour])])
+            for neighbour in self.directed[vertex]:
+                red_lines.append((coordinates[vertex], coordinates[neighbour]))
+        line_collection = matplotlib.collections.LineCollection(lines)
+        red_line_collection = matplotlib.collections.LineCollection(red_lines, colors="red")
+        green_line_collection = matplotlib.collections.LineCollection(green_lines, colors="#42c432")
+        ax.add_collection(line_collection)
+        ax.add_collection(red_line_collection)
+        ax.add_collection(green_line_collection)
+        pyplot.scatter([coordinates[vertex][0] for vertex in coordinates if vertex not in highlighted_node],
+                       [coordinates[vertex][1] for vertex in coordinates if vertex not in highlighted_node])
+        pyplot.scatter([coordinates[vertex][0] for vertex in highlighted_node],
+                       [coordinates[vertex][1] for vertex in highlighted_node], c='#42c432')
+        for i, vertex in enumerate(coordinates):
+            pyplot.annotate(vertex, (coordinates[vertex][0], coordinates[vertex][1]))
+        if save:
+            pyplot.savefig(save)
+        if show:
+            pyplot.show()
+        pyplot.close()
--- tests/src/graph/test_mixed_tree.py
+++ tests/src/graph/test_mixed_tree.py
@@ -1,5 +1,6 @@
 from TBS.tree import random_tree
 from TBS.graph.binary_mixed_tree import BinaryMixedTree
+from TBS.graph import Graph
 import unittest
 
 
@@ -83,3 +84,26 @@ class TestMixedTree(unittest.TestCase):
         self.assertEqual({frozenset([9])}, mixed.directed[frozenset([3])])
         self.assertEqual(set(frozenset([x]) for x in [5, 6]), mixed.undirected[frozenset([3])])
         self.assertEqual(set(frozenset([x]) for x in [0, 1, 4, 8]), mixed.undirected[frozenset([9])])
+
+    def test_to_graph(self):
+        mixed = BinaryMixedTree({})
+        for i in range(7):
+            mixed.add_vertex(i)
+        mixed.add_directed(3, 6)
+        mixed.add_directed(2, 5)
+        mixed.add_undirected(0, 1)
+        mixed.add_undirected(0, 2)
+        mixed.add_undirected(0, 3)
+        mixed.add_undirected(1, 4)
+        tree = Graph(vertices=[0, 1, 2, 3, 4, 5, 6], edges=((0, 1), (0, 2), (0, 3), (1, 4), (2, 5), (3, 6)))
+        self.assertEqual(tree, mixed.to_graph())
+
+    def test_find_root(self):
+        tree = Graph(vertices=[0, 1, 2, 3, 4, 5, 6], edges=((0, 1), (0, 2), (0, 3), (1, 4), (2, 5), (3, 6)))
+        binary_mixed_tree = BinaryMixedTree.from_tree(tree)
+        root = binary_mixed_tree.find_root_as_undirected()
+        self.assertEqual(root, 0)
+        tree = Graph(vertices=[0, 1, 2, 3, 4, 5], edges=((0, 1), (0, 2), (0, 3), (1, 4), (1, 5)))
+        binary_mixed_tree = BinaryMixedTree.from_tree(tree)
+        root = binary_mixed_tree.find_root_as_undirected()
+        self.assertTrue(root == 0 or root == 1)
